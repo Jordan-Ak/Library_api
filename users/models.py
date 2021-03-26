bf7a5006c4135c009e58d6212104023c6bdf9063
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-26 13:25:59 +0100

    Edited name fields and altered id field.

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ import uuid
 # Create your models here.
 
 class CustomUser(AbstractUser): #Current model for custom user
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = CharField(max_length = 100, blank=False, null = False)
-    last_name = CharField(max_length = 100, blank=False, null = False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,)
+    first_name = models.CharField(max_length = 100, blank=False, null = False,)
+    last_name = models.CharField(max_length = 100, blank=False, null = False,)
     email = models.EmailField(max_length = 254, blank = False, null = False, unique = True,)
     phone_no = models.CharField(max_length = 30, blank=True, null=True,)
