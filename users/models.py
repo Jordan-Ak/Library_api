8f36c27142ee5b95bb8efbafa3b5c2f5ae6c60ca
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-26 12:18:46 +0100

    Bug fixes.

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(max_length = 254, required =True, unique = True **options)
-    phone_no = models.CharField(max_length = 30, blank=True, null=True)
+    email = models.EmailField(max_length = 254, required =True, unique = True,)
+    phone_no = models.CharField(max_length = 30, blank=True, null=True,)
