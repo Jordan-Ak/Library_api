d478d2a993daac5cc386f64c4f97467e07eec71e
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-26 19:37:40 +0100

    Made database edits.
    Made a git reset to edit the database.

--- library/settings.py
+++ library/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', #Remember to install psycopg2 (pip install psycopg2)
-        'NAME': 'library', #This name has to be the exact name as the database you setup in pgadmin 4
+        'NAME': 'library1', #This name has to be the exact name as the database you setup in pgadmin 4
         'USER': env.str('USER'),#Default username for postgresql after install postgresql
         'PASSWORD': env.str('PASSWORD'),#Remember the password you used during installation.
         'HOST': '127.0.0.1', #Host server as the name says.
