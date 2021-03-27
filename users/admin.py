4ad04b528aa553c3cffaa4794332d804fc316265
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-27 11:08:38 +0100

    Dropped unnecessary complexity to focus on productivity.
    This involved editing models and creating a new database.

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
 #Configuration to allow proper viewing in admin site
-CustomUser = get_user_model()
 
-class CustomUserAdmin(UserAdmin):    
-    add_form = CustomUserCreationForm    
-    form = CustomUserChangeForm    
-    model = CustomUser    
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+      
     list_display = ['id','email', 'username','is_staff',]
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,14 @@
-from django.contrib.auth import get_user_model
+from django import forms
+from .models import CustomUser
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 #Forms added for easy manipulation in admin
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:        
-        model = get_user_model()        
-        fields = ('first_name', 'last_name','username','email', 'phone_no')
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = get_user_model()
-        fields = ('first_name', 'last_name','username','email', 'phone_no')
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
