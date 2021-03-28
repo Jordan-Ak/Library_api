9c89cd55b275cc1cf0b2879a99abd23d5a0fb948
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-03-28 15:23:15 +0100

    Code refactoring and adding appropriate comments.

--- library/urls.py
+++ library/urls.py
@@ -23,14 +23,24 @@ from users.views import CustomPasswordTokenVerificationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   
+    #This is to verify email after registration
     path('dj-rest-auth/registration/account-confirm-email/<str:key>/',
-        ConfirmEmailView.as_view()),
-    path('dj-rest-auth/password/reset/', include('django_rest_passwordreset.urls',)),
-    path('password/reset/verify-token/', CustomPasswordTokenVerificationView.as_view()),
+        ConfirmEmailView.as_view()), 
+
+    #Third party package django password reset to reset passwords with email
+    path('dj-rest-auth/password/reset/', include('django_rest_passwordreset.urls',)), 
+    path('password/reset/verify-token/', CustomPasswordTokenVerificationView.as_view()), 
+    #Verify password reset tokens   
+    
+    #Third party package rest-auths for user registration and authentication with email account activation
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api-auth', include('rest_framework.urls')),
     path('dj-rest-auth/registration/account-confirm-email/', EmailVerificationSentView.as_view(),
         name='account_email_verification_sent'),
+
+    #To enable quick login and logout restframework
+    path('api-auth', include('rest_framework.urls')),
+    
     
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,4 @@
 from rest_framework import serializers
-from django.utils.translation import ugettext_lazy as _
 
 class CustomTokenSerializer(serializers.Serializer):
-    token = serializers.CharField()
-
-class PasswordTokenSerializer(serializers.Serializer):
-    password = serializers.CharField(label=_("Password"), style={'input_type': 'password'})
     token = serializers.CharField()
