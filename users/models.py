41db0f34b2bc380bac8ec1a95ea0645d4d62e068
unknown <JosiahDavid98@gmail.com>
2021-04-01 03:41:15 -0700

    Switched to a new device

--- users/models.py
+++ users/models.py
@@ -7,4 +7,4 @@ import uuid
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,)
     email = models.EmailField(max_length = 254, blank = False, null = False, unique = True,)
-    
+    #
